1f25990bb9a397755655d3b23396abca6de8cf3e
Natalene35 <poupouille20@msn.com>
2024-09-11 10:02:58 +0200

    Test sur le formulaire de création de sortie failed

--- tests/SortieControllerTest.php
+++ tests/SortieControllerTest.php
@@ -72,4 +72,34 @@ class SortieControllerTest extends WebTestCase
             'sortie[lieu]' => '3',
         ]);
     }
+
+    public function testFormAjoutSortieFailed()
+    {
+        $client = static::createClient();
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->find(1);
+
+        //On simule l'authentification de cet utilisateur
+        $client->loginUser($testUser);
+
+        // Aller à la page du formulaire d'ajout de sortie
+        $client->request('GET', '/sortie/create');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Enregistrer', [
+            'sortie[dateSortie]' => '2024-09-15 12:30:00',
+            'sortie[dateFinInscription]' => '2024-09-14 22:30:00',
+            'sortie[nombrePlace]' => '5',
+            'sortie[duree]' => '90',
+            'sortie[description]' => 'On mange des pizzas au parc quoi',
+            'sortie[campus]' => '2',
+            'sortie[ville]' => '2',
+            'sortie[lieu]' => '3',
+        ]);
+
+        $this->assertSelectorTextContains('.invalid-feedback', 'Le nom doit être renseigné');
+    }
 }
