c2f71ad90121c1db19aec71b80511c326e3441a8
Jules SENOT <jules.senot2023@campus-eni.fr>
2024-09-11 11:59:42 +0200

    fix password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,10 +5,12 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Validator\UpdatePassword;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,7 +23,27 @@ class UserType extends AbstractType
         $builder
             ->add('email', EmailType::class)
 //            ->add('roles')
-            ->add('password', PasswordType::class, ['label'=>'Mot de passe', 'required' => false,'mapped' => false])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new UpdatePassword()
+                ],
+                'first_options'  => [
+                    'label'=>'Mot de passe', 
+                    'attr' => array(
+                        'placeholder' => '******'
+                    ),
+                ],
+                'second_options' => [
+                    'label'=>'Répéter le mot de passe',
+                    'attr' => array(
+                        'placeholder' => '******'
+                    ),
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+                ])
             ->add('pseudo', TextType::class)
             ->add('prenom', TextType::class)
             ->add('nom', TextType::class)
