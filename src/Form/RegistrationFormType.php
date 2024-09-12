0989df459993753eaaf39bf06c0faa33d811f74a
Gabriel DOMERGUE <gabriel.domergue2023@campus-eni.fr>
2024-09-12 10:02:03 +0200

    regex email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,9 +9,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -22,16 +22,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email',EmailType::class)
+            ->add('email', TextType::class, [
+                'constraints' => [
+                new Assert\Regex([
+                    'pattern' => '/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/',
+                    'message' => 'Email non valide',
+                ]),
+            ],
+            ])
             ->add('prenom',TextType::class)
             ->add('pseudo',TextType::class)
             ->add('nom',TextType::class)
-            ->add('telephone',TelType::class)
+            ->add('telephone',TextType::class)
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'nom',
             ])
-            ->add('plainPassword', RepeatedType::class, [
+            ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'required' => false,
                 'mapped' => false,
